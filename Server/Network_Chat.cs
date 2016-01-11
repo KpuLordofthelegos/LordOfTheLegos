3eca82d2be4d5fd4b4a7e62b18eee4c8a1dda8c8
moiras2 <rahae0320@gmail.com>
2016-01-11 18:19:57 +0900

    1st week

---
+++ Server/Network_Chat.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Network_Chat : Photon.MonoBehaviour {
+    public static Network_Chat SP;
+    public List<string> messages = new List<string>();
+
+    private int chatHeight = (int)140;
+    private Vector2 scrollPos = Vector2.zero;
+    private string chatInput = "";
+
+    void Awake()
+    {
+        SP = this;
+    }
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, Screen.height - chatHeight, Screen.width, chatHeight));
+
+        scrollPos = GUILayout.BeginScrollView(scrollPos);
+        GUI.color = Color.black;
+        for(int i = messages.Count - 1;i>=0;i--)
+        {
+            GUILayout.Label(messages[i]);
+        }
+        GUILayout.EndScrollView();
+        GUI.color = Color.white;
+        chatInput = GUILayout.TextField(chatInput);
+
+        GUILayout.BeginHorizontal();
+        GUI.color = Color.black;
+        GUILayout.Label("Send to:", GUILayout.Width(60));
+        GUI.color = Color.white;
+        if (GUILayout.Button("ALL", GUILayout.Height(17)))
+            SendChat(PhotonTargets.All);
+        if (GUILayout.Button("ALLBUF", GUILayout.Height(17)))
+            SendChat(PhotonTargets.AllBuffered);
+        if (GUILayout.Button("OTHER", GUILayout.Height(17)))
+            SendChat(PhotonTargets.Others);
+        if (GUILayout.Button("OTHERBUF", GUILayout.Height(17)))
+            SendChat(PhotonTargets.OthersBuffered);
+        if (GUILayout.Button("MASTER", GUILayout.Height(17)))
+            SendChat(PhotonTargets.MasterClient);
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+    }
+    public static void AddMessage(string text)
+    {
+        SP.messages.Add(text);
+        if (SP.messages.Count > 15)
+            SP.messages.RemoveAt(0);
+    }
+    void SendChat(PhotonTargets target)
+    {
+        photonView.RPC("SendChatMessage", target, chatInput);
+        chatInput = "";
+    }
+    [RPC]
+    void SendChatMessage(string text,PhotonMessageInfo info)
+    {
+        AddMessage("[" + info.sender + "]" + text);
+    }
+	// Use this for initialization
+}
---
+++ Server/Network_Connect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Network_Connect : Photon.MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.isVisible = true;
+        ro.isOpen = true;
+        ro.maxPlayers = 8;
+        ro.customRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "", 0 } };
+        PhotonNetwork.CreateRoom("", ro, TypedLobby.Default);
+    }
+    void OnJoinedRoom()
+    {
+        Debug.Log("OK!");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
